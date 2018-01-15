c01ac8201bc47cf6a7f0bf3d68cdec9264ee4c2d
PINTO Dani <dani@MBP-de-PINTO.g4-paris>
2018-01-15 15:43:04 +0100

    Bouton Play

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-responsiveVoice.speak("hello world");
+//responsiveVoice.speak("Voulez vous coucher avec moi ce soir ?", "French Female");
+var test =  document.getElementById("article");
+var test = test.innerText;
